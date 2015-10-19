4f9cc97f26de152d69f0dd3352d5383ae7690356
ta5aka1 <cba27ag@gmail.com>
2015-10-19 16:42:30 +0000

    practice c.

---
+++ cpractice/part1/2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void printHelloWorld(char *str) {
+    printf("%s\n", str);
+}
+
+int main(void) {
+    printHelloWorld("Hello World");
+}
---
+++ cpractice/part1/3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print1() {
+    printf("ふるいけいや\n");
+}
+void print2() {
+    printf("かわずとびこむ\n");
+}
+void print3() {
+    printf("みずのおと\n");
+}
+
+void main(){
+    print1();
+    print2();
+    print3();
+}
