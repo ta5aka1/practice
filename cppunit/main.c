fd222e7dadc1b1e1100d9ba187359f48491e2a60
ta5aka1 <cba27ag@gmail.com>
2015-11-05 16:17:15 +0000

    add test

--- cppunit/main.c
+++ cppunit/main.c
@@ -3,6 +3,8 @@
 
 void sort(int array[], int num);
 void test_sort_001(void);
+void test_sort_002(void);
+void test_sort_003(void);
 
 int main() {
     CU_pSuite sort_suite;
@@ -10,6 +12,8 @@ int main() {
     CU_initialize_registry();
     sort_suite = CU_add_suite("Sort", NULL, NULL);
     CU_add_test(sort_suite, "test_001", test_sort_001);
+    CU_add_test(sort_suite, "test_002", test_sort_002);
+    CU_add_test(sort_suite, "test_003", test_sort_003);
     CU_console_run_tests();
     CU_cleanup_registry();
     
@@ -37,4 +41,25 @@ void test_sort_001 (void) {
     
     sort(array, 1);
     CU_ASSERT(array[0] == 3);
+}
+
+void test_sort_002 (void) {
+    int array[] = {11, 7, 5, 3, 2};
+    sort(array, 5);
+    CU_ASSERT(array[0] == 2);
+    CU_ASSERT(array[1] == 3);
+    CU_ASSERT(array[2] == 5);
+    CU_ASSERT(array[3] == 7);
+    CU_ASSERT(array[4] == 11);
+}
+
+void test_sort_003 (void) {
+    int array[] = {7,11,3,2,5};
+    
+    sort(array, 5);
+    CU_ASSERT(array[0] == 2);
+    CU_ASSERT(array[1] == 3);
+    CU_ASSERT(array[2] == 5);
+    CU_ASSERT(array[3] == 7);
+    CU_ASSERT(array[4] == 11);
 }
