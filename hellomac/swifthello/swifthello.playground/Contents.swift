8fc663c7177f4c5ae182993f77edc8424623befc
Takayasu Sakai <cba27ag@gmail.com>
2015-11-16 02:30:41 +0900

    hello swift

---
+++ hellomac/swifthello/swifthello.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let aaa = 1
+let bbb = 2
+let ccc = aaa + bbb
+
+let ddd = "Rainbow"
+let eee = "Apps"
+let fff = ddd + eee
+
+for i in 1...50{
+    i/10    
+}
