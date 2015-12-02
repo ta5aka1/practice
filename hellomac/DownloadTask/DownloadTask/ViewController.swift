3b65ac2088e058d21dc46980a11ca97287997429
Takayasu Sakai <cba27ag@gmail.com>
2015-12-02 23:58:55 +0900

    modify image download process to be able to process in background

--- hellomac/DownloadTask/DownloadTask/ViewController.swift
+++ hellomac/DownloadTask/DownloadTask/ViewController.swift
@@ -11,13 +11,34 @@ import UIKit
 class ViewController: UIViewController , NSURLSessionDownloadDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var showBtn: UIButton!
+    @IBOutlet weak var startBtn: UIButton!
+    var rawData : NSData?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.lightGrayColor()
+        self.showBtn.hidden = true
     }
 
     @IBAction func tap(sender: AnyObject) {
-        self.downloadWithDataTask()
+        let identifier = "BackgroundTaskIdentifier"
+        let configuration = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier(identifier)
+        
+        let session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        
+        let url = NSURL(string: "http://www.geocities.jp/ezotombo9/Sample/syouzyou.jpg")
+        let task = session.downloadTaskWithURL(url!)
+        task.resume()
+    }
+    
+    @IBAction func showImage(sender: AnyObject) {
+        if self.rawData != nil {
+            self.imageView.image = UIImage(data:self.rawData!)
+        }
+        
     }
     
     func downloadWithDataTask() {
@@ -40,7 +61,8 @@ class ViewController: UIViewController , NSURLSessionDownloadDelegate {
             NSLog("download Error!")
         }else{
             NSLog("download Success!")
-            self.imageView.image = UIImage(data: data!)
+            self.rawData = data
+            self.showBtn.hidden = false
         }
     }
     
