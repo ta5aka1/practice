f4a00482d6131cff544a06a399d378439b0f79b4
Takayasu Sakai <cba27ag@gmail.com>
2015-12-05 21:40:21 +0900

    pinch application add

---
+++ hellomac/PinchInOutApp/PinchInOutApp/GestureUIImageView.swift
@@ -0,0 +1,9 @@
+//
+//  GestureUIImageView.swift
+//  PinchInOutApp
+//
+//  Created by Takayasu Sakai on 2015/12/05.
+//  Copyright © 2015年 Takayasu Sakai. All rights reserved.
+//
+
+import Foundation
---
+++ hellomac/PinchInOutApp/PinchInOutApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinchInOutApp
+//
+//  Created by Takayasu Sakai on 2015/12/05.
+//  Copyright © 2015年 Takayasu Sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
