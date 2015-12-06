485884d161476eded7a4fff4d1d5baf24cba13a4
Takayasu Sakai <cba27ag@gmail.com>
2015-12-06 12:41:44 +0900

    update pinch app

---
+++ hellomac/PinchInOutApp/PinchInOutApp/ImageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ImageViewController.swift
+//  PinchInOutApp
+//
+//  Created by Takayasu Sakai on 2015/12/06.
+//  Copyright © 2015年 Takayasu Sakai. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
