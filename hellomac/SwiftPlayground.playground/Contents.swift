b4acf3e3fe36163b389adf48006080f6f903d42d
Takayasu Sakai <cba27ag@gmail.com>
2015-12-10 01:20:29 +0900

    play swift.

---
+++ hellomac/SwiftPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("hello world")
+print("バッテリー残量が少なくなっています")
+print(1==1)
+print( 1 == 2 )
+
+var bodyTemp:float_t = 36
+bodyTemp = 36.5
+
