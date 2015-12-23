2446359cb323f24caa32f20041c4ddb35afaf01e
Takayasu Sakai <cba27ag@gmail.com>
2015-12-23 13:57:13 +0900

    add fibonacci generator

--- hellomac/underscoresample.playground/Contents.swift
+++ hellomac/underscoresample.playground/Contents.swift
@@ -9,3 +9,32 @@ let fansOfGakki = fans.filter{(_, target) in
 }
 
 print(fansOfGakki)
+
+
+struct FibonacciGenerator: GeneratorType{
+    typealias Element = Int
+    var (a,b) = (0,1)
+    mutating func next() -> Element? {
+        let n = a
+        (a,b) = (b, a+b)
+        return n
+    }
+}
+
+var fib = FibonacciGenerator()
+fib.next()
+fib.next()
+fib.next()
+fib.next()
+fib.next()
+
+struct FibonacciSequence: SequenceType {
+    typealias Generator = FibonacciGenerator
+    func generate() -> Generator {
+        return Generator()
+    }
+}
+
+for (i, n) in zip(0..<10, FibonacciSequence()) {
+    print("F_\(i) = \(n)")
+}
